607feae9628600904e102941d48299d28fc669a3
iloverobloxone-art <iloveroblox.one@gmail.com>
2025-10-18 12:30:43 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,47 +1,67 @@
 // server.js
 const express = require('express');
 const http = require('http');
-const socketio = require('socket.io');
-
+const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = new Server(server);
 
-let rooms = {};
+app.use(express.static('public')); // Serve your Three.js client (index.html, game.js, assets in /public)
 
-io.on('connection', (socket) => {
-  let roomId = null;
-  let playerName = null;
+const gameState = {
+    players: {},
+    teams: { red: [], blue: [], green: [], yellow: [] },
+    blocks: [],
+    beds: { red: true, blue: true, green: true, yellow: true }
+    // You'd add more for resources, merchants, upgrades, etc.
+};
 
-  socket.on('join', ({room, name}) => {
-    roomId = room;
-    playerName = name;
-    if (!rooms[roomId]) rooms[roomId] = [];
-    if (rooms[roomId].length < 4) {
-      rooms[roomId].push({id: socket.id, name: playerName});
-      socket.join(roomId);
-      io.to(roomId).emit('player-list', rooms[roomId]);
-    } else {
-      socket.emit('full');
-    }
-  });
+io.on('connection', socket => {
+    // Assign a team to new player (round robin for demo)
+    let teamOrder = ['red', 'blue', 'green', 'yellow'];
+    let teamName = teamOrder[Math.floor(Math.random() * 4)];
+    gameState.teams[teamName].push(socket.id);
+    gameState.players[socket.id] = {
+        id: socket.id,
+        team: teamName,
+        position: { x: 0, y: 2, z: 15 },
+        alive: true
+    };
+    socket.emit('init', { id: socket.id, team: teamName, gameState });
 
-  socket.on('game-event', (msg) => {
-    // Broadcast game state changes, moves, bed destroyed, etc.
-    socket.to(roomId).emit('game-event', msg);
-  });
+    // Broadcast new player to others
+    socket.broadcast.emit('player-join', gameState.players[socket.id]);
 
-  socket.on('disconnect', () => {
-    if (roomId && rooms[roomId]) {
-      rooms[roomId] = rooms[roomId].filter(p => p.id !== socket.id);
-      io.to(roomId).emit('player-list', rooms[roomId]);
-      if (rooms[roomId].length === 0) delete rooms[roomId];
-    }
-  });
+    // Handle player position/movement updates
+    socket.on('move', pos => {
+        if (gameState.players[socket.id]) {
+            gameState.players[socket.id].position = pos;
+            socket.broadcast.emit('player-move', { id: socket.id, pos });
+        }
+    });
+
+    // Handle block placement
+    socket.on('place-block', data => {
+        gameState.blocks.push(data); // data: { team, position }
+        io.emit('update-block', data);
+    });
+
+    // Handle bed interactions
+    socket.on('bed-break', team => {
+        gameState.beds[team] = false;
+        io.emit('bed-status', { team, broken: true });
+    });
+
+    // Disconnect
+    socket.on('disconnect', () => {
+        delete gameState.players[socket.id];
+        for (let team in gameState.teams) {
+            gameState.teams[team] = gameState.teams[team].filter(pid => pid !== socket.id);
+        }
+        io.emit('player-leave', socket.id);
+    });
 });
 
-app.use(express.static('public')); // Serve your client files from ./public
-
 server.listen(3000, () => {
-  console.log('Bedwars server running on http://localhost:3000');
+    console.log('Bedwars 3D server running on http://localhost:3000');
 });
